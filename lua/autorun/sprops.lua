3794db2638903a22b096e99d9b26c35d807fd957
shadowscion <shadowscion@users.noreply.github.com>
2021-02-10 14:42:42 -0600

    Update sprops.lua

--- lua/autorun/sprops.lua
+++ lua/autorun/sprops.lua
@@ -35,18 +35,18 @@ list.Add("OverrideMaterials", "sprops/trans/misc/beam_side")
 
 if SERVER then
 	---------------------------------------------------------------
-    -- CUSTOM PA OFFSETS
-    list.Set("PA_mirror_exceptions", "models/sprops/trans/wheel_d", Angle(180, 180, 0))
-    list.Set("PA_mirror_exceptions", "models/sprops/trans/wheel_b", Angle(180, 180, 0))
-    list.Set("PA_mirror_exceptions", "models/sprops/trans/miscwheels", Angle(180, 180, 0))
-    list.Set("PA_mirror_exceptions", "models/sprops/trans/train", Angle(180, 180, 0))
-    list.Set("PA_mirror_exceptions", "models/sprops/trans/lights", Angle(0, 180, 180))
+	-- CUSTOM PA OFFSETS
+	list.Set("PA_mirror_exceptions", "models/sprops/trans/wheel_d", Angle(180, 180, 0))
+	list.Set("PA_mirror_exceptions", "models/sprops/trans/wheel_b", Angle(180, 180, 0))
+	list.Set("PA_mirror_exceptions", "models/sprops/trans/miscwheels", Angle(180, 180, 0))
+	list.Set("PA_mirror_exceptions", "models/sprops/trans/train", Angle(180, 180, 0))
+	list.Set("PA_mirror_exceptions", "models/sprops/trans/lights", Angle(0, 180, 180))
 
-    list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_1.mdl", Angle(0, 180, 180))
-    list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_2.mdl", Angle(0, 180, 180))
-    list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_3.mdl", Angle(0, 180, 180))
-    list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/train/track_t90_01.mdl", Angle(180, 90, 0))
-    list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/train/track_t90_02.mdl", Angle(180, 90, 0))
+	list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_1.mdl", Angle(0, 180, 180))
+	list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_2.mdl", Angle(0, 180, 180))
+	list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_3.mdl", Angle(0, 180, 180))
+	list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/train/track_t90_01.mdl", Angle(180, 90, 0))
+	list.Set("PA_mirror_exceptions_specific", "models/sprops/trans/train/track_t90_02.mdl", Angle(180, 90, 0))
 
 	return
 end
